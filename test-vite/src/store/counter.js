401dbb0f7e67870a121c87daeff579b6a1f1fac7
Francisco Mizael Parra Iturralde <franciscomizaparra.24@gmail.com>
2022-10-19 08:57:10 -0500

    Carpeta front end agregada.

---
+++ test-vite/src/store/counter.js
@@ -0,0 +1,23 @@
+const counterModule = {
+    state: {
+        count: 0
+    },
+    mutations: {
+        SOCKET_COUNTER_INCREMENT (state, counter){
+            state.count = counter
+        },
+        SOCKET_COUNTER_DECREMENT (state, counter){
+            state.count = counter
+        }
+    },
+    actions: {
+        socket_increment: ({state, rootState}) => {
+            rootState.io.emit('increment', state.count)
+        },
+        socket_decrement: ({state, rootState}) => {
+            rootState.io.emit('decrement', state.count)
+        }
+    }
+}
+
+export default counterModule
